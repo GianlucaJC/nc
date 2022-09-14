1d1f4d77bbe7af96f38a37291e74b71b438c0185
GianlucaJC <morescogianluca@gmail.com>
2022-09-14 12:15:54 +0200

    Public database example

---
+++ database_example.php
@@ -0,0 +1,47 @@
+<?php
+class Database{
+
+	private $nomehost='localhost';
+	private $db_name = "nc";
+	private $nomeuser='root';
+	private $password='';	
+	
+	private $nomehostT='localhost';
+	private $db_nameT = "LIOFILCHEM";
+	private $nomeuserT='';
+	private $passwordT='';
+
+	
+	public $conn;
+	
+	public function getConnection() {
+		$this->conn = null;
+		try
+			{
+				$this->conn = new mysqli($this->nomehost,$this->nomeuser,$this->password,$this->db_name);
+			}
+		catch(mysqli $exception)
+			{
+			echo "Errore di connessione: " . $exception->getMessage();
+			}
+		return $this->conn;
+		
+	}
+	
+	public function getConnTarget() {
+		$this->conn = null;
+		try
+			{
+				$this->conn = new mysqli($this->nomehostT,$this->nomeuserT,$this->passwordT,$this->db_nameT);
+			}
+		catch(mysqli $exception)
+			{
+			echo "Errore di connessione: " . $exception->getMessage();
+			}
+		return $this->conn;
+		
+	}	
+}
+
+	
+?>
