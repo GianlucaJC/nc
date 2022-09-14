da0fe023405b728595c96e37b8204023ddedf4e1
GianlucaJC <morescogianluca@gmail.com>
2022-09-14 11:27:23 +0200

    Update gitignore

--- database.php
+++ database.php
@@ -5,16 +5,28 @@ class Database{
 	private $nomehost='localhost';
 	private $db_name = "nc";
 	private $nomeuser='root';
-	private $password='';	
+	private $password='giatongia6971';	
 	
 	private $nomehostT='localhost';
 	private $db_nameT = "LIOFILCHEM";
 	private $nomeuserT='root';
-	private $passwordT='';
+	private $passwordT='giatongia6971';
+	
 
+	//Liofilchem
+	/*
+	private $nomehost='192.168.129.30';
+	private $db_name="nc";
+	private $nomeuser='sysadmin';
+	private $password='Password01.';
+
+
+	private $nomehostT='192.168.129.20';
+	private $db_nameT = "LIOFILCHEM";
+	private $nomeuserT='jolly';
+	private $passwordT='zxcvbnm';
 
 	*/
-	
 	
 	public $conn;
 	
